7e187d372def67f38a8fe8cd60cbdb240cfdc534
NebraskyTheWolf <farfy.dev@gmail.com>
2023-05-27 22:53:48 +0200

    fix(src/): Fixing authentication middleware, JWT SIGNATURE

--- src/server/middleware/Authentication.ts
+++ src/server/middleware/Authentication.ts
@@ -11,18 +11,29 @@ class Authentication extends BaseMiddleware {
         if (isNull(token))
             return response.status(403).json({
                 status: false,
-                error: "INVALID_TOKEN"
+                error: "AUTHENTICATION_REQUIRED",
+                message: "Please authenticate to use the resources."
             }).end();
     
-        const decoded = jwt.verify(token, ServerManager.instance.environement.read<string>("JWT_SECRET_KEY"))
-        if (isTypeNull<CustomRequest>(decoded))
-            return response.status(500).json({
-                status: false,
-                error: "Invalid account data or signature does not match."
-            }).end();
-        
-        request.token = decoded
-        return next();
+        jwt.verify(token, ServerManager.instance.environement.read<string>("JWT_SECRET_KEY").replaceAll('-', '-'), (error, decoded) => {
+
+            if (error) {
+                return response.status(403).json({
+                    status: false,
+                    error: error.cause,
+                    message: error.message
+                }).end();
+            }
+
+            if (isTypeNull<CustomRequest>(decoded))
+                return response.status(500).json({
+                    status: false,
+                    error: "Invalid account data or signature does not match."
+                }).end();
+            
+            request.token = decoded
+            return next();
+        }) 
     }
 }
 
